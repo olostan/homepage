86a595a8f79949b038c5e0bff308a39a4de95210
Valetyn Shybanov <olostan@gmail.com>
2021-03-24 03:15:37 -0700

    Updates

--- build.js
+++ build.js
@@ -1,5 +1,6 @@
 const less = require('less');
 const fs = require('fs');
+const puppeteer = require('puppeteer');
 
 function buildCss() {
     less.render(fs.readFileSync('public/clear.less').toString(),
@@ -17,16 +18,19 @@ function buildCss() {
 }
 
 const execFile = require('child_process').execFile;
-const CHROME = '/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome';
- 
-function buildPDF(debug) {
-    return new Promise((resolve, reject) => {
-        let url = debug?'http://localhost:5000':'https://olostan.name/';
-        execFile(CHROME, ['--headless', '--disable-gpu', '--print-to-pdf=public/Valentyn\ Shybanov\ Personal\ profile.pdf', url],function(err){
-            console.log("PDF built");
-            return true;
-        });
-    });
+
+async function buildPDF(debug) {
+    const browser = await puppeteer.launch({ headless: true });
+    //const CHROME = '/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome';
+    const page = await browser.newPage();
+
+    await page.goto(debug?'http://localhost:5000':'https://olostan.name/', {waitUntil: 'networkidle0'});
+    await page.pdf({ path: 'public/Valentyn\ Shybanov\ Personal\ profile.pdf',
+         margin: { top: "40", right: "40", bottom: "40", left: "40" },
+         displayHeaderFooter: true, 
+         format: 'A4' });
+    await browser.close();
+    console.log("PDF created");
 }
 
 if (process.argv[2]!='watch') {
@@ -36,20 +40,25 @@ if (process.argv[2]!='watch') {
     console.log("watching...");
     buildCss();
     buildPDF(true);
+    let pdfTimer = null;
+    function rebuildPDF() {
+        if (pdfTimer!=null) return;
+        pdfTimer = setTimeout(() => {
+            buildPDF(true).then(() => pdfTimer = false).catch((err) => console.error(err));
+        });
+    }
     fs.watch('public/clear.less',{},function (eventType, filename)  {
         if (eventType=='change') {
             console.log("Css changed");
             buildCss();
+            buildPDF(true);
         }
     });
-    let pdfTimer = null;
+   
     fs.watch('public/index.html',{},function (eventType, filename)  {        
         if (eventType=='change') {
-            if (pdfTimer!=null) return;
-            pdfTimer = setTimeout(() => {
-                console.log("PDF changed");
-                buildPDF(true).then(() => pdfTimer = false);
-            });
+            console.log("HTML changed");
+            buildPDF(true);
         }
     });
 
